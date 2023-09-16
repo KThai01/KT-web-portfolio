70de67b01a9dbd1439dcda51f55db8c1ca007c53
KThai01 <kevinathai01@gmail.com>
2023-09-16 16:13:16 -0700

    Checkpoint 4
    Particles working on a single page.
    Moved particle options to a separate file to increase legibility of page.js file.

---
+++ kt-web-portfolio/src/app/components/Particles.js
@@ -0,0 +1,87 @@
+import Particles from 'react-tsparticles';
+
+export default function ParticleBG() {
+    return (
+        <Particles
+              options={{
+                fullScreen: {
+                  enable: false,
+                  zIndex: -1,
+                },
+                style: {
+                    position: "absolute",
+                    height: "100%",
+                },
+                particles: {
+                  number: {
+                    value: 100,
+                    limit: 100,
+                    density: {
+                      enable: true,
+                      value_area: 500
+                    }
+                  },
+                  color: {
+                    value: "#ffffff"
+                  },
+                  shape: {
+                    type: "edge",
+                    stroke: {
+                      width: 0,
+                      color: "#059669"
+                    },
+                    polygon: {
+                      nb_sides: 5
+                    },
+                    image: {
+                      src: "images/github.svg",
+                      width: 5,
+                      height: 5
+                    }
+                  },
+                  opacity: {
+                    value: 0.5,
+                    random: true,
+                    anim: {
+                      enable: true,
+                      speed: 1,
+                      opacity_min: 0.5,
+                      sync: false
+                    }
+                  },
+                  size: {
+                    value: 1,
+                    random: true,
+                    anim: {
+                      enable: true,
+                      speed: 5,
+                      size_min: 7,
+                      sync: false
+                    }
+                  },
+                  line_linked: {
+                    enable: true,
+                    distance: 170,
+                    color: "#059669",
+                    opacity: 1,
+                    width: 1
+                  },
+                  move: {
+                    enable: true,
+                    speed: 1,
+                    direction: "none",
+                    random: false,
+                    straight: false,
+                    out_mode: "out",
+                    bounce: false,
+                    attract: {
+                      enable: false,
+                      rotateX: 600,
+                      rotateY: 1200
+                    }
+                  }
+                },
+              }}
+            />
+    );
+}
--- kt-web-portfolio/src/app/page.js
+++ kt-web-portfolio/src/app/page.js
@@ -1,10 +1,7 @@
 'use client'
-
 import Head from "next/head";
 import { AiFillLinkedin, AiFillGithub, AiFillMail } from 'react-icons/ai'
-
-import Particles from 'react-tsparticles';
-
+import ParticleBG from './components/Particles';
 
 export default function Home() {
   return (
@@ -13,121 +10,44 @@ export default function Home() {
         <title>Kevin Thai Portfolio</title>
       </Head>
 
-      <main className="bg-gradient-to-b from-slate-900 to-gray-700">
-        <div>
+      <main>
+        <div className="bg-gradient-to-b from-slate-900 to-gray-700">
+          <ParticleBG />
           <section className="min-h-screen">
-            <nav className="py-10 mb-12 flex justify-end">
-              <ul className="flex items-center">
-                <li className="text-3xl flex gap-5"> 
-                  <AiFillLinkedin />
-                  <AiFillGithub />
-                  <AiFillMail />
-                </li>
-                <li><a className="bg-gradient-to-tl from-sky-800 to-emerald-400 text-white font-mono px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
-              </ul>
-            </nav>
-
+            <div>
+              <nav className="py-10 px-10 mb-12 flex justify-end">
+                <ul className="flex items-center">
+                  <li className="text-4xl flex gap-5"> 
+                    <AiFillLinkedin />
+                    <AiFillGithub />
+                    <AiFillMail />
+                  </li>
+                  <li><a className="bg-gradient-to-tl from-sky-800 to-emerald-400 text-white font-sans px-4 py-2 rounded-md ml-8" href="#">Resume</a></li>
+                </ul>
+              </nav>  
+            </div>
+          
             <div className="flex justify-center items-center">
               <div className="text-center p-10">
-                <h2 className="text-5xl py-2 font-mono">Hello! I'm <span className="text-emerald-600">Kevin Thai</span></h2>
-                <h3 className="text-4xl py-2 font-mono">Full Stack Developer</h3>
-                <p className="font-mono">Student at <span className="text-emerald-600">California State University Long Beach</span></p>
-                <p className="font-mono py-2">Graduating <span className="text-emerald-600">December 2023</span></p>
+                <h2 className="text-6xl py-2 font-sans">Hello! I'm <span className="text-emerald-600">Kevin Thai</span></h2>
+                <h3 className="text-5xl py-2 font-sans">Full Stack Developer</h3>
+                <p className="font-sans">Student at <span className="text-emerald-600">California State University Long Beach</span></p>
+                <p className="font-sans py-2">Graduating <span className="text-emerald-600">December 2023</span></p>
               </div>
             </div>
-
-              <Particles
-                options={{
-                  fullScreen: {
-                    enable: true,
-                    zIndex: 0
-                  },
-                  particles: {
-                    number: {
-                      value: 100,
-                      limit: 100,
-                      density: {
-                        enable: true,
-                        value_area: 500
-                      }
-                    },
-                    color: {
-                      value: "#ffffff"
-                    },
-                    shape: {
-                      type: "circle",
-                      stroke: {
-                        width: 0,
-                        color: "#000000"
-                      },
-                      polygon: {
-                        nb_sides: 5
-                      },
-                      image: {
-                        src: "images/github.svg",
-                        width: 10,
-                        height: 10
-                      }
-                    },
-                    opacity: {
-                      value: 0.5,
-                      random: true,
-                      anim: {
-                        enable: true,
-                        speed: 1,
-                        opacity_min: 0.5,
-                        sync: false
-                      }
-                    },
-                    size: {
-                      value: 10,
-                      random: true,
-                      anim: {
-                        enable: true,
-                        speed: 10,
-                        size_min: 10,
-                        sync: false
-                      }
-                    },
-                    line_linked: {
-                      enable: true,
-                      distance: 100,
-                      color: "#ffffff",
-                      opacity: 1,
-                      width: 1
-                    },
-                    move: {
-                      enable: true,
-                      speed: 1,
-                      direction: "none",
-                      random: false,
-                      straight: false,
-                      out_mode: "out",
-                      bounce: false,
-                      attract: {
-                        enable: false,
-                        rotateX: 600,
-                        rotateY: 1200
-                      }
-                    }
-                  },
-                }}
-              />
           </section>
         </div>
 
-        <section className="min-h-screen">
-          <div>
-            <h3 className="text-4xl text-emerald-600 font-mono justif-center font-bold">Skills</h3>
-            <p className="text-xl">Skills I've picked up through school and projects</p>
-          </div>
-
-          <div>
-            <div className="text-center p-10">
-              <img src="https://assets.stickpng.com/images/5847ea22cef1014c0b5e4833.png" alt="Angular" width={100} height={100}></img>
+        <div className="min-h-screen bg-gray-900">
+          <section>
+            <div>
+              <h3 className="text-4xl text-emerald-600 font-sans justif-center font-bold px-10 py-5">Skills</h3>
             </div>
-          </div>
-        </section>
+            <div className="text-center p-10">
+                
+            </div>
+          </section>
+        </div>
       </main>
     </div>
   )
